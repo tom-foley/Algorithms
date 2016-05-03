f2a776429a10dd7b02960f0deb46d2fca198f40c
tom-foley <foley.236@osu.edu>
2016-05-02 20:54:34 -0400

    Added & Implemented HackerRanks "Extra Long Factorials" Challenge

---
+++ Algorithms/HackerRank/ExtraLongFactorials/ExtraLongFactorials.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Numerics;
+
+namespace Algorithms.HackerRank.ExtraLongFactorials
+{
+    class ExtraLongFactorials
+    {
+        static BigInteger Factorial(int n)
+        {
+            if (n > 1)
+            {
+                return n * Factorial(n - 1);
+            }
+
+            return n;
+        }
+
+
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Factorial(n));
+        }
+    }
+}
