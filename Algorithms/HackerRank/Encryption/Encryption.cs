360dcafa3fecd0b12b8566e697de68c9ec9ff9f0
tom-foley <foley.236@osu.edu>
2016-05-03 20:44:43 -0400

    Added & implemented HackerRanks "Encryption" challenge

---
+++ Algorithms/HackerRank/Encryption/Encryption.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Text;
+
+namespace Algorithms.HackerRank.Encryption
+{
+    class Encryption
+    {
+        static string Encrypt(string plaintext)
+        {
+            string condensed = plaintext.Replace(" ", "");
+            StringBuilder sb = new StringBuilder();
+
+            int pos;
+            int len = condensed.Length;
+            int rows = Convert.ToInt32(Math.Floor(Math.Sqrt(len)));
+            int cols = Convert.ToInt32(Math.Ceiling(Math.Sqrt(len)));
+
+            //  Alternate between incrementing rows & columns
+            byte alt = 0x00;
+            while (rows * cols < len)
+            {
+                if (alt == 0x00 && rows < cols)
+                {
+                    rows++;
+                    alt = 0x01;
+                }
+                else
+                {
+                    cols++;
+                    alt = 0x00;
+                }
+            }
+
+            for (int i = 0; i < cols; i++)
+            {
+                pos = i;
+                for (int j = 0; j < rows; j++)
+                {
+                    if (pos < len)
+                    {
+                        sb.Append(condensed[pos]);
+                        pos += cols;
+                    }
+                }
+
+                sb.Append(' ');
+            }
+
+            return sb.ToString();
+        }
+
+
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            Console.WriteLine(Encrypt(s));
+            //Console.Read();
+        }
+    }
+}
